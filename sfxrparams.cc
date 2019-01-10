45aa2a88ebda8bdacab754b79ffb3efe340d25b9
Gustav <sir.gustav.the.coder@gmail.com>
2019-01-10 20:16:54 +0100

    split into source and header

--- sfxrparams.cc
+++ sfxrparams.cc
@@ -2,6 +2,8 @@
 #include <string>
 #include <cmath>
 
+#include "sfxrparams.h"
+
 namespace Synthesizer   
 {
 	/**
@@ -23,122 +25,45 @@ namespace Synthesizer
 	 * 
 	 * @author Thomas Vian
 	 */
-	class SfxrParams 
-	{
-		//--------------------------------------------------------------------------
-		//
-		//  Properties
-		//
-		//--------------------------------------------------------------------------
 		
-    public:
-
-		/** If the parameters have been changed since last time (shouldn't used cached sound) */
-		bool paramsDirty = true;			
-		
-		//interface uses this to disable square sliders when non-square wavetype selected
-		// public static const SquareParams:Array = ["squareDuty","dutySweep"];
-		
-		//params to exclude from list
-		// public static const ExcludeParams:Array = ["waveType","masterVolume"];
-		
-    enum class WaveType
-    {
-      Square,
-      Saw,
-      Sin,
-      Noise,
-      Triangle,
-      Pink,
-      Tan,
-      Whistle,
-      Breaker,
-      COUNT
-    };
-			
-    template<typename T>
-    struct Param {
-      std::string real_name;
-      std::string description;
-      T default_value;
-      T min_value;
-      T max_value;
-      T random_power;
-
-      T get() const { return current_value; }
-      void set(T t)
-      {
-        if(t > max_value) current_value = max_value;
-        else if(t < min_value) current_value = min_value;
-        else current_value = t;
-      }
-
-      T current_value;
-      bool locked;
-    };
-    // std::vector<Param*> all_params;
-
-		// real name, decription, grouping, default, min, max, 
-		Param<double> waveType = Param<double>{"Wave Type","Shape of the wave.", 0,2,0,WAVETYPECOUNT-0.0001}; // the 6.999 thing is because this is really an int parameter...		
-		
-		Param<double> masterVolume = Param<double>{"Master Volume","Overall volume of the sound.", 1,0.5,0,1}; 	
-		Param<double> attackTime = Param<double>{"Attack Time","Length of the volume envelope attack.", 1,0,0,1};		
-		Param<double> sustainTime = Param<double>{"Sustain Time","Length of the volume envelope sustain.", 1,0.3,0,1}; 	
-		Param<double> sustainPunch = Param<double>{"Punch","Tilts the sustain envelope for more 'pop'.", 1,0,0,1}; 		
-		Param<double> decayTime = Param<double>{"Decay Time","Length of the volume envelope decay (yes, I know it's called release).", 1,0.4,0,1}; 	
-		
-		Param<double> compressionAmount = Param<double>{"Compression","Pushes amplitudes together into a narrower range to make them stand out more.  Very good for sound effects, where you want them to stick out against background music.", 15,0.3,0,1};
-		
-		Param<double> startFrequency = Param<double>{"Frequency","Base note of the sound.", 2,0.3,0,1}; 		
-		Param<double> minFrequency = Param<double>{"Frequency Cutoff","If sliding, the sound will stop at this frequency, to prevent really low notes.  If unlocked, this is set to zero during randomization.", 2,0.0,0,1}; 		
-		
-		Param<double> slide = Param<double>{"Frequency Slide","Slides the frequency up or down.", 3,0.0,-1,1}; 	
-		Param<double> deltaSlide = Param<double>{"Delta Slide","Accelerates the frequency slide.  Can be used to get the frequency to change direction.", 3,0.0,-1,1}; 		
-		
-		Param<double> vibratoDepth = Param<double>{"Vibrato Depth","Strength of the vibrato effect.", 4,0,0,1}; 		
-		Param<double> vibratoSpeed = Param<double>{"Vibrato Speed","Speed of the vibrato effect (i.e. frequency).", 4,0,0,1}; 		
-		
-		Param<double> overtones = Param<double>{"Harmonics","Overlays copies of the waveform with copies and multiples of its frequency.  Good for bulking out or otherwise enriching the texture of the sounds (warning: this is the number 1 cause of bfxr slowdown!).", 13,0,0,1}; 		
-		Param<double> overtoneFalloff = Param<double>{"Harmonics Falloff","The rate at which higher overtones should decay.", 13,0,0,1}; 
-		
-		Param<double> changeRepeat = Param<double>{"Pitch Jump Repeat Speed","Larger Values means more pitch jumps, which can be useful for arpeggiation.", 5,0,0,1}; 		
-		
-		Param<double> changeAmount = Param<double>{"Pitch Jump Amount 1","Jump in pitch, either up or down.", 5,0,-1,1}; 		
-		Param<double> changeSpeed = Param<double>{"Pitch Jump Onset 1","How quickly the note shift happens.", 5,0,0,1}; 		
-		
-		Param<double> changeAmount2 = Param<double>{"Pitch Jump Amount 2","Jump in pitch, either up or down.", 5,0,-1,1}; 	
-		Param<double> changeSpeed2 = Param<double>{"Pitch Jump Onset 2","How quickly the note shift happens.", 5,0,0,1}; 		
-		
-		Param<double> squareDuty = Param<double>{"Square Duty","Square waveform only : Controls the ratio between the up and down states of the square wave, changing the tibre.", 8,0,0,1}; 		
-		Param<double> dutySweep = Param<double>{"Duty Sweep","Square waveform only : Sweeps the duty up or down.", 8,0,-1,1}; 		
-		
-		Param<double> repeatSpeed = Param<double>{"Repeat Speed","Speed of the note repeating - certain variables are reset each time.", 9,0,0,1}; 	
-		
-		Param<double> flangerOffset = Param<double>{"Flanger Offset","Offsets a second copy of the wave by a small phase, changing the tibre.", 10,0,-1,1}; 		
-		Param<double> flangerSweep = Param<double>{"Flanger Sweep","Sweeps the phase up or down.", 10,0,-1,1}; 
-		
-		Param<double> lpFilterCutoff = Param<double>{"Low-pass Filter Cutoff","Frequency at which the low-pass filter starts attenuating higher frequencies.  Named most likely to result in 'Huh why can't I hear anything?' at her high-school grad. ", 11,1,0,1}; 		
-		Param<double> lpFilterCutoffSweep = Param<double>{"Low-pass Filter Cutoff Sweep","Sweeps the low-pass cutoff up or down.", 11,0,-1,1}; 	
-		Param<double> lpFilterResonance = Param<double>{"Low-pass Filter Resonance","Changes the attenuation rate for the low-pass filter, changing the timbre.", 11,0,0,1}; 		
-		
-		Param<double> hpFilterCutoff = Param<double>{"High-pass Filter Cutoff","Frequency at which the high-pass filter starts attenuating lower frequencies.", 12,0,0,1}; 	
-		Param<double> hpFilterCutoffSweep = Param<double>{"High-pass Filter Cutoff Sweep","Sweeps the high-pass cutoff up or down.", 12,0,-1,1}; 	
-						
-		Param<double> bitCrush = Param<double>{"Bit Crush","Resamples the audio at a lower frequency.", 14,0,0,1};
-		Param<double> bitCrushSweep = Param<double>{"Bit Crush Sweep","Sweeps the Bit Crush filter up or down.", 14,0,-1,1}; 
-		
-		//--------------------------------------------------------------------------
-		//
-		//  Getters / Setters
-		//
-		//--------------------------------------------------------------------------
-		
-		static constexpr int WAVETYPECOUNT = static_cast<int>(WaveType::COUNT);
-		
-		SfxrParams()
+  SfxrParams::SfxrParams()
+      // real name, decription, grouping, default, min, max, 
+      : waveType ( Param<double>{"Wave Type","Shape of the wave.", 0,2,0,WAVETYPECOUNT-0.0001}) // the 6.999 thing is because this is really an int parameter...		
+      , masterVolume ( Param<double>{"Master Volume","Overall volume of the sound.", 1,0.5,0,1}) 	
+      , attackTime ( Param<double>{"Attack Time","Length of the volume envelope attack.", 1,0,0,1})		
+      , sustainTime ( Param<double>{"Sustain Time","Length of the volume envelope sustain.", 1,0.3,0,1}) 	
+      , sustainPunch ( Param<double>{"Punch","Tilts the sustain envelope for more 'pop'.", 1,0,0,1}) 		
+      , decayTime ( Param<double>{"Decay Time","Length of the volume envelope decay (yes, I know it's called release).", 1,0.4,0,1}) 	
+      , compressionAmount ( Param<double>{"Compression","Pushes amplitudes together into a narrower range to make them stand out more.  Very good for sound effects, where you want them to stick out against background music.", 15,0.3,0,1})
+      , startFrequency ( Param<double>{"Frequency","Base note of the sound.", 2,0.3,0,1}) 		
+      , minFrequency ( Param<double>{"Frequency Cutoff","If sliding, the sound will stop at this frequency, to prevent really low notes.  If unlocked, this is set to zero during randomization.", 2,0.0,0,1}) 		
+      , slide ( Param<double>{"Frequency Slide","Slides the frequency up or down.", 3,0.0,-1,1}) 	
+      , deltaSlide ( Param<double>{"Delta Slide","Accelerates the frequency slide.  Can be used to get the frequency to change direction.", 3,0.0,-1,1}) 		
+      , vibratoDepth ( Param<double>{"Vibrato Depth","Strength of the vibrato effect.", 4,0,0,1}) 		
+      , vibratoSpeed ( Param<double>{"Vibrato Speed","Speed of the vibrato effect (i.e. frequency).", 4,0,0,1}) 		
+      , overtones ( Param<double>{"Harmonics","Overlays copies of the waveform with copies and multiples of its frequency.  Good for bulking out or otherwise enriching the texture of the sounds (warning: this is the number 1 cause of bfxr slowdown!).", 13,0,0,1}) 		
+      , overtoneFalloff ( Param<double>{"Harmonics Falloff","The rate at which higher overtones should decay.", 13,0,0,1}) 
+      , changeRepeat ( Param<double>{"Pitch Jump Repeat Speed","Larger Values means more pitch jumps, which can be useful for arpeggiation.", 5,0,0,1}) 		
+      , changeAmount ( Param<double>{"Pitch Jump Amount 1","Jump in pitch, either up or down.", 5,0,-1,1}) 		
+      , changeSpeed ( Param<double>{"Pitch Jump Onset 1","How quickly the note shift happens.", 5,0,0,1}) 		
+      , changeAmount2 ( Param<double>{"Pitch Jump Amount 2","Jump in pitch, either up or down.", 5,0,-1,1}) 	
+      , changeSpeed2 ( Param<double>{"Pitch Jump Onset 2","How quickly the note shift happens.", 5,0,0,1}) 		
+      , squareDuty ( Param<double>{"Square Duty","Square waveform only : Controls the ratio between the up and down states of the square wave, changing the tibre.", 8,0,0,1}) 		
+      , dutySweep ( Param<double>{"Duty Sweep","Square waveform only : Sweeps the duty up or down.", 8,0,-1,1}) 		
+      , repeatSpeed ( Param<double>{"Repeat Speed","Speed of the note repeating - certain variables are reset each time.", 9,0,0,1}) 	
+      , flangerOffset ( Param<double>{"Flanger Offset","Offsets a second copy of the wave by a small phase, changing the tibre.", 10,0,-1,1}) 		
+      , flangerSweep ( Param<double>{"Flanger Sweep","Sweeps the phase up or down.", 10,0,-1,1}) 
+      , lpFilterCutoff ( Param<double>{"Low-pass Filter Cutoff","Frequency at which the low-pass filter starts attenuating higher frequencies.  Named most likely to result in 'Huh why can't I hear anything?' at her high-school grad. ", 11,1,0,1}) 		
+      , lpFilterCutoffSweep ( Param<double>{"Low-pass Filter Cutoff Sweep","Sweeps the low-pass cutoff up or down.", 11,0,-1,1}) 	
+      , lpFilterResonance ( Param<double>{"Low-pass Filter Resonance","Changes the attenuation rate for the low-pass filter, changing the timbre.", 11,0,0,1}) 		
+      , hpFilterCutoff ( Param<double>{"High-pass Filter Cutoff","Frequency at which the high-pass filter starts attenuating lower frequencies.", 12,0,0,1}) 	
+      , hpFilterCutoffSweep ( Param<double>{"High-pass Filter Cutoff Sweep","Sweeps the high-pass cutoff up or down.", 12,0,-1,1}) 	
+      , bitCrush ( Param<double>{"Bit Crush","Resamples the audio at a lower frequency.", 14,0,0,1})
+      , bitCrushSweep ( Param<double>{"Bit Crush Sweep","Sweeps the Bit Crush filter up or down.", 14,0,-1,1}) 
 		{
 			//initialize param object 
 			resetParams();
+
 
       attackTime.random_power = 4;
       sustainTime.random_power = 2;
@@ -157,7 +82,7 @@ namespace Synthesizer
       bitCrushSweep.random_power = 5;
 		}
 
-		void setAllLocked(bool locked)
+		void SfxrParams::setAllLocked(bool locked)
     {
       auto params = GetParams();
       for(auto* p: params)
@@ -166,7 +91,7 @@ namespace Synthesizer
       }
     }
 		
-    std::vector<Param<double>*> GetParams()
+    std::vector<SfxrParams::Param<double>*> SfxrParams::GetParams()
 		{
       std::vector<Param<double>*> ret;
 
@@ -215,7 +140,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate a pickup/coin sound
 		 */
-		void generatePickupCoin()
+		void SfxrParams::generatePickupCoin()
 		{
 			resetParams();
 			
@@ -239,7 +164,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate a laser/shoot sound
 		 */
-		void generateLaserShoot()
+		void SfxrParams::generateLaserShoot()
 		{
 			resetParams();
 			
@@ -292,7 +217,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate an explosion sound
 		 */
-		void generateExplosion()
+		void SfxrParams::generateExplosion()
 		{
 			resetParams();
 			waveType.set( 3);
@@ -333,7 +258,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate a powerup sound
 		 */
-		void generatePowerup()
+		void SfxrParams::generatePowerup()
 		{
 			resetParams();
 			
@@ -365,7 +290,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate a hit/hurt sound
 		 */
-		void generateHitHurt()
+		void SfxrParams::generateHitHurt()
 		{
 			resetParams();
 			
@@ -387,7 +312,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate a jump sound
 		 */
-		void generateJump()
+		void SfxrParams::generateJump()
 		{
 			resetParams();
 			
@@ -406,7 +331,7 @@ namespace Synthesizer
 		/**
 		 * Sets the parameters to generate a blip/select sound
 		 */
-		void generateBlipSelect()
+		void SfxrParams::generateBlipSelect()
 		{
 			resetParams();
 			
@@ -424,14 +349,14 @@ namespace Synthesizer
 		/**
 		 * Resets the parameters, used at the start of each generate function
 		 */
-		void resetParams()
+		void SfxrParams::resetParams()
 		{
 			paramsDirty = true;
 			
       auto params = GetParams();
       for(auto* p: params)
       {
-        p->current_value = p->default_value;
+        p->set( p->default_value);
         p->locked = false;
       }
 			masterVolume.locked = true;
@@ -446,7 +371,7 @@ namespace Synthesizer
 		/**
 		 * Randomly adjusts the parameters ever so slightly
 		 */
-		void mutate(double mutation = 0.05)
+		void SfxrParams::mutate(double mutation)
 		{			
       auto params = GetParams();
 			for (auto* param : params)
@@ -465,7 +390,7 @@ namespace Synthesizer
 		 * Sets all parameters to random values
 		 * If passed null, no fields constrained
 		 */
-		void randomize()
+		void SfxrParams::randomize()
 		{
 			auto params = GetParams();
 			for (auto* param : params)
@@ -536,5 +461,4 @@ namespace Synthesizer
 				}
 			}
 		}
-  };
 }
