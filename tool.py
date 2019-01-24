5bd8110eeb104fa9c6274b002170c63f5f555e0f
Gustav <sir.gustav.the.coder@gmail.com>
2019-01-24 08:08:35 +0100

    use python to unzip

--- tool.py
+++ tool.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 import argparse
 import urllib.request
-
+import zipfile
 
 def handle_download(args):
     print('Downloading', args.url)
     print('Target', args.target)
     urllib.request.urlretrieve(args.url, args.target)
     print('Downloaded.')
+
+
+def handle_unzip(args):
+    print('Unzipping', args.zip)
+    print('Target', args.target)
+    zip = zipfile.ZipFile(args.zip, 'r')
+    zip.extractall(args.target)
+    zip.close()
+    print('Extracted')
 
 
 def main():
@@ -19,6 +28,11 @@ def main():
     download_parser.set_defaults(handler=handle_download)
     download_parser.add_argument('url')
     download_parser.add_argument('target')
+
+    zip_parser = subparsers.add_parser('unzip')
+    zip_parser.set_defaults(handler=handle_unzip)
+    zip_parser.add_argument('zip')
+    zip_parser.add_argument('target')
 
     args = parser.parse_args()
     if args.handler is None:
