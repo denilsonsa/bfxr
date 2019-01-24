950467d9ba18cbcd275e120347e06c00a749b05d
Gustav <sir.gustav.the.coder@gmail.com>
2019-01-24 08:02:29 +0100

    tool.py now prints status and accepts action argument

--- tool.py
+++ tool.py
@@ -3,12 +3,28 @@ import argparse
 import urllib.request
 
 
+def handle_download(args):
+    print('Downloading', args.url)
+    print('Target', args.target)
+    urllib.request.urlretrieve(args.url, args.target)
+    print('Downloaded.')
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('url')
-    parser.add_argument('target')
+    parser.set_defaults(handler=None)
+    subparsers = parser.add_subparsers()
+
+    download_parser = subparsers.add_parser('download')
+    download_parser.set_defaults(handler=handle_download)
+    download_parser.add_argument('url')
+    download_parser.add_argument('target')
+
     args = parser.parse_args()
-    urllib.request.urlretrieve(args.url, args.target)
+    if args.handler is None:
+        parser.print_help()
+    else:
+        args.handler(args)
 
 if __name__ == "__main__":
     main()
